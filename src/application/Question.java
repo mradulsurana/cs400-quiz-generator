6c57b06b1c17f56a29fdcd6ebe0179f9665f8454
achang38 <achang38@wisc.edu>
2019-04-24 17:52:58 -0500

    change Question and Quiz

--- src/application/Question.java
+++ src/application/Question.java
@@ -5,6 +5,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
 import static javafx.application.Application.launch;
+
+import java.util.ArrayList;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,93 +20,31 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-public class Question extends Application {
-  static int count;
-  static int correct;
-  public Question() {
-    count++;
-  }
-@Override
-public void start(Stage primaryStage) {
-  BorderPane root = new BorderPane();
-//show question number
-primaryStage.setTitle("Test Question "+count);
-
-//label for question text
-Label labelQuestion = new Label("What is the answer to question "+count+"?");
-
-
-Button button= new Button("Next");
-
-RadioButton choice1, choice2, choice3, choice4;
-choice1 = new RadioButton("A");
-choice2 = new RadioButton("B");
-choice3 = new RadioButton("C");
-choice4 = new RadioButton("D");
-
-ToggleGroup question1= new ToggleGroup();
-
-choice1.setToggleGroup(question1);
-choice2.setToggleGroup(question1);
-choice3.setToggleGroup(question1);
-choice4.setToggleGroup(question1);
-
-button.setDisable(true);
-
-choice1.setOnAction(e -> button.setDisable(false) );
-choice2.setOnAction(e -> button.setDisable(false) );
-choice3.setOnAction(e -> button.setDisable(false) );
-choice4.setOnAction(e -> button.setDisable(false) );
-
-button.setOnAction(e -> 
-{
-  if(choice1.isSelected() ) {
-    correct++;
-  }
-  if(count <5) {
-Question next = new Question();
-next.start(primaryStage);
-  }
-  else {
-    Results result = new Results(count,correct);
-    result.start(primaryStage);
-  };
-});
-
-//create image 
-Image placeholder = new Image("placeholder.png");
-//put image into ImageView object
-ImageView i1 = new ImageView();
-i1.setImage(placeholder);
-//set size of image
-i1.setFitWidth(200);
-//maintain ratio of image
-i1.setPreserveRatio(true);
-//keep image quality
-i1.setSmooth(true);
-i1.setCache(true);
-//put image in center pane
-root.setCenter(i1);
-
-//holds question and choices and button
-VBox layout= new VBox(5);
-layout.getChildren().addAll(labelQuestion, choice1, choice2, choice3, choice4, button);
-
-//put question in center, image on top
-root.setCenter(layout);
-root.setTop(i1);
-
-//align the nodes in root
-root.setAlignment(i1, Pos.CENTER);
-root.setMargin(layout, new Insets(50));
-root.setMargin(i1, new Insets(50));
-Scene scene1= new Scene(root, 700, 700);
-primaryStage.setScene(scene1);
-        
-primaryStage.show();
-}
-
+public class Question {
+  private String question;
+  private Image image;
+  public ArrayList<String> correctAns;
+  public ArrayList<String> incorrectAns;
   
-
-    
+  public Question(String question) {
+    this.question = question;
+    this.correctAns = new ArrayList<String>();
+    this.incorrectAns = new ArrayList<String>();
+  }
+  
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+  
+  public void setImage(Image image) {
+    this.image = image;
+  }
+  
+  public String getQuestion() {
+    return this.question;
+  }
+  
+  public Image getImage() {
+    return this.image;
+  }
 }
---
+++ src/application/Quiz.java
@@ -0,0 +1,149 @@
+package application;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+import static javafx.application.Application.launch;
+
+import java.util.ArrayList;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Quiz extends Application {
+  private static ArrayList<Question> questions;
+  private ArrayList<RadioButton> toggles;
+  private ArrayList<RadioButton> correctToggles;
+  private static int count;
+  private static int correct;
+  private static int max = 5;
+  
+  public Quiz() {
+    if(count == 0) { 
+    this.questions = new ArrayList<Question>();
+    Question testQuestion = new Question("What is the answer to the question?"); 
+    testQuestion.correctAns.add("A");
+    testQuestion.incorrectAns.add("B");
+    testQuestion.incorrectAns.add("C");
+    testQuestion.incorrectAns.add("D");
+    this.questions.add(testQuestion);
+    }
+    count++;
+  }
+  
+  public void setNumQuestions(int num) {
+    this.max = num;
+  }
+  private void setToggle() {
+    Question testQuestion = this.questions.get(0);
+    this.toggles = new ArrayList<RadioButton>();
+    this.correctToggles = new ArrayList<RadioButton>();
+    for(String c : testQuestion.correctAns) {
+      RadioButton button = new RadioButton(c);
+      this.toggles.add(button);
+      this.correctToggles.add(button);
+    }
+    for(String i : testQuestion.incorrectAns) {
+      this.toggles.add(new RadioButton(i));
+    }
+  }
+  
+   private ImageView createImage(String imageFile) {
+  //create image 
+    Image placeholder = new Image("placeholder.png");
+    //put image into ImageView object
+    ImageView i1 = new ImageView();
+    i1.setImage(placeholder);
+    //set size of image
+    i1.setFitWidth(200);
+    //maintain ratio of image
+    i1.setPreserveRatio(true);
+    //keep image quality
+    i1.setSmooth(true);
+    i1.setCache(true);
+    return i1;
+  }
+
+@Override
+public void start(Stage primaryStage) {
+  this.setNumQuestions(10);
+  this.setToggle();
+  
+  BorderPane root = new BorderPane();
+  //show question number
+  primaryStage.setTitle("Test Question "+count);
+
+  //label for question text
+  Label labelQuestion = new Label("What is the answer to question "+count+"?");
+
+  Question testQuestion = this.questions.get(0);
+
+  Button button= new Button("Next");
+  button.setDisable(true);
+
+  ToggleGroup question1= new ToggleGroup();
+
+  for(RadioButton r: this.toggles) {
+    r.setToggleGroup(question1);
+    r.setOnAction(e -> button.setDisable(false));
+  }
+
+  button.setOnAction(e -> 
+  {
+    for(RadioButton r2: this.toggles) {
+      if(this.correctToggles.contains(r2)) {
+        if(r2.isSelected() ) {
+          correct++;
+          break;
+        }
+      }
+    }
+    if(count < max) {
+      Quiz next = new Quiz();
+      next.start(primaryStage);
+    }
+    else {
+      Results result = new Results(count,correct);
+      result.start(primaryStage);
+    };
+  });
+
+  ImageView i1 = this.createImage("test");
+  //put image in center pane
+  root.setCenter(i1);
+
+  //holds question and choices and button
+  VBox layout= new VBox(5);
+  layout.getChildren().add(labelQuestion);
+  for(RadioButton r3: this.toggles) {
+    layout.getChildren().add(r3);
+  }
+  layout.getChildren().add(button);
+  //put question in center, image on top
+  root.setCenter(layout);
+  root.setTop(i1);
+
+  //align the nodes in root
+  root.setAlignment(i1, Pos.CENTER);
+  root.setMargin(layout, new Insets(50));
+  root.setMargin(i1, new Insets(50));
+  Scene scene1= new Scene(root, 700, 700);
+  primaryStage.setScene(scene1);
+        
+  primaryStage.show();
+}
+
+  
+
+    
+}
