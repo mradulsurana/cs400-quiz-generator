e949045bcebc8c93ff4f9a4222fdc87b1a80efb8
achang38 <achang38@wisc.edu>
2019-04-24 18:53:57 -0500

    CSS

--- src/application/Results.java
+++ src/application/Results.java
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 package application;
 
 import javafx.application.Application;
@@ -54,68 +53,9 @@ public class Results extends Application{
     root.setBottom(h);
     root.setMargin(v, new Insets(100));
     root.setMargin(h, new Insets(100));
-    Scene scene2 = new Scene(root,1400,864);
+    Scene scene2 = new Scene(root,1400,86);
     scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
     primaryStage.setScene(scene2);
     primaryStage.show();
-=======
-package application;
-
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-public class Results extends Application{
-  private int total;
-  private int correct;
-  
-  public Results(int total, int correct) {
-    this.total = total;
-    this.correct = correct;
-  }
-  
-  public Results() {
-    this.total = 0;
-    this.correct = 0;
-  }
-  
-  public double getPercent() {
-    return (double)correct/total;
-  }
-  
-  public int getCorrect() {
-    return correct;
-  }
-  
-  public int getIncorrect() {
-    return total-correct;
-  }
-  
-  @Override
-  public void start(Stage primaryStage) {
-    BorderPane root = new BorderPane();
-    VBox v = new VBox(5);
-    HBox h = new HBox(5);
-    Label correct = new Label("Questions correct: "+this.getCorrect());
-    Label incorrect = new Label("Questions incorrect: "+this.getIncorrect());
-    Label percent = new Label("Percent: "+this.getPercent());
-    Button makeNew = new Button("Make new quiz");
-    Button exit = new Button("Exit");
-    v.getChildren().addAll(correct,incorrect,percent);
-    h.getChildren().addAll(makeNew,exit);
-    root.setCenter(v);
-    root.setBottom(h);
-    root.setMargin(v, new Insets(100));
-    root.setMargin(h, new Insets(100));
-    Scene scene2 = new Scene(root,700,700);
-    primaryStage.setScene(scene2);
-    primaryStage.show();
->>>>>>> 1703589be15d5fb37f2dbaec80cf94547c150f44
   }
 }
