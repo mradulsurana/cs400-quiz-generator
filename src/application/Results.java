b52772d40b1d526f4ed28651ae1dc5078f853070
achang38 <achang38@wisc.edu>
2019-04-24 19:15:12 -0500

    button functionality

--- src/application/Results.java
+++ src/application/Results.java
@@ -2,6 +2,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,10 +15,12 @@ import javafx.stage.Stage;
 public class Results extends Application{
   private int total;
   private int correct;
+  private Main mainScene;
   
-  public Results(int total, int correct) {
+  public Results(Main mainScene,int total, int correct) {
     this.total = total;
     this.correct = correct;
+    this.mainScene = mainScene;
   }
   
   public Results() {
@@ -39,22 +42,28 @@ public class Results extends Application{
   
   @Override
   public void start(Stage primaryStage) {
+    
     BorderPane root = new BorderPane();
+    Scene scene2 = new Scene(root,1400,86);
+    scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
     VBox v = new VBox(5);
     HBox h = new HBox(5);
     Label correct = new Label("Questions correct: "+this.getCorrect());
     Label incorrect = new Label("Questions incorrect: "+this.getIncorrect());
     Label percent = new Label("Percent: "+this.getPercent());
     Button makeNew = new Button("Make new quiz");
+    makeNew.setOnAction(e -> {
+      this.mainScene.subStart(primaryStage);
+    });
     Button exit = new Button("Exit");
+    exit.setOnAction(e -> Platform.exit());
     v.getChildren().addAll(correct,incorrect,percent);
     h.getChildren().addAll(makeNew,exit);
     root.setCenter(v);
     root.setBottom(h);
     root.setMargin(v, new Insets(100));
     root.setMargin(h, new Insets(100));
-    Scene scene2 = new Scene(root,1400,86);
-    scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
     primaryStage.setScene(scene2);
     primaryStage.show();
   }
