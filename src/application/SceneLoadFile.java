a486f5f5b395f6035d2aa94b0e8f67c3f5ac1785
holmbeck <holmbeck@wisc.edu>
2019-04-28 12:52:15 -0500

    json parsing initial code

--- src/application/SceneLoadFile.java
+++ src/application/SceneLoadFile.java
@@ -12,11 +12,14 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
@@ -34,15 +37,20 @@ public class SceneLoadFile extends Application {
 	Stage primaryStage;
 	BorderPane root;
 	ArrayList<Question> questions;
+	ArrayList<String> topicsStr;
+	ObservableList<String> topics;
 
 	public SceneLoadFile(Main mainScene) {
 		this.mainScene = mainScene;
+		primaryStage = null;
 		questions = new ArrayList();
 		root = new BorderPane();
+		topicsStr = new ArrayList<String>();
+		topics = FXCollections.observableArrayList();
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws FileNotFoundException, IOException, ParseException {
 
 		this.primaryStage = primaryStage;
 		primaryStage.setTitle("Load Quiz Data");
@@ -50,26 +58,51 @@ public class SceneLoadFile extends Application {
 		Scene sceneLoad = new Scene(root, 1000, 800);
 		sceneLoad.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-		buildLeft();
+		buildMiddle();
 		buildBottom();
 		primaryStage.setScene(sceneLoad);
 		primaryStage.show();
 
 	}
 
-	public ArrayList<Question> getQuizData() {
+	public ArrayList<Question> getQuestions() {
 		return questions;
+	}
+	
+	public ObservableList<String> getTopics() {
+		return topics;
 	}
 
 	private void parseQuestions(String filePath) throws Exception {
-		Object obj = new JSONParser().parse(new FileReader(filePath));
-		JSONObject jo = (JSONObject) obj;
-		JSONArray qArr = (JSONArray) jo.get("questionArray");
-
-		// TODO parse together the questions
+		Object obj = new JSONParser().parse(new FileReader(filePath)); // parses .json to an Object
+		JSONObject jo = (JSONObject) obj; //casts to JSONObject
+		JSONArray qArr = (JSONArray) jo.get("questionArray"); // gets array of questions
+		
+		for(int i = 0; i < qArr.size(); ++i) { // iterates through each question
+			
+			JSONObject jQuestion = (JSONObject) qArr.get(i); // the question as a JSONObject
+			Question question = new Question((String)jQuestion.get("questionText")); // adds text of question
+			
+			question.setTopic((String)jQuestion.get("topic")); // adds the question topic
+			
+			File childJson = new File(filePath); // .json as file object
+			String parentDir = childJson.getParent(); // parent dir which image should be in
+			Image qImage = new Image(parentDir + (String)jQuestion.get("image")); // image should be in same directory
+			question.setImage(qImage); // connects the image to the question
+			
+			JSONArray choices = (JSONArray) jQuestion.get("choiceArray"); // choices to parse
+			for(int j = 0; j < choices.size(); j++) { // iterate through choices
+				JSONObject choice = (JSONObject) choices.get(i); // the given choice
+				question.getAllAns().add((String) choice.get("choice")); // text of the given choice
+				if(((String)choice.get("isCorrect")).equals("T")) { // if correct, adds to correct ans list in question
+					question.getCorrectAns().add((String)choice.get("isCorrect"));
+				}
+			}
+			
+		}
 	}
 
-	private void buildLeft() {
+	private void buildMiddle() {
 		VBox leftBox = new VBox(30);
 		leftBox.setId("VBox");
 
