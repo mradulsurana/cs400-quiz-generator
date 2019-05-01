ec05882dc82b9d7f11e1fa10fc6814a92ee2e71c
holmbeck <holmbeck@wisc.edu>
2019-05-01 17:00:04 -0500

    file header comment on SceneLoadFile

--- src/application/SceneLoadFile.java
+++ src/application/SceneLoadFile.java
@@ -1,3 +1,23 @@
+//////////////////////////////// HEAEDER COMMENT //////////////////////////////
+//
+// Title: cs400-quiz-generator
+// Course: (CS 400 section 001, Spring, 2019)
+// Due: May 2nd by 10:00pm
+//
+// Author: (Alfred Holmbeck, Mradul Surana, Allen Chang, Michael Lyrek, Jordan Ingbretson)
+// Lecturer's Name: (Prof. KUEMMEL)
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Persons: (none)
+// Online Sources: (none)
+//
+////////////////////////// COMMENTS and|or KNOWN BUGS /////////////////////////
+//
+// (none)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 package application;
 
 import java.io.File;
@@ -35,6 +55,11 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
+/**
+ * 
+ * @author Holms31
+ *
+ */
 public class SceneLoadFile extends Application {
 
 	Main mainScene; // instance to go back to when the files have been loaded
